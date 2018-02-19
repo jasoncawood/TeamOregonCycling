b9697d66eb9809fb24ca37df6fdb6f3b2393dba5
John Wilger <john@johnwilger.com>
2018-02-19 18:57:27 +0000

    Allow options to be specified for markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,14 +20,16 @@ module ApplicationHelper
     " - #{@page_title}"
   end
 
-  def markdown(text)
-    options = {
+  def markdown(text, **options)
+    default_options = {
       filter_html:     true,
       hard_wrap:       true,
       link_attributes: { rel: 'nofollow', target: "_blank" },
       space_after_headers: true,
       fenced_code_blocks: true
     }
+
+    options = default_options.merge(options)
 
     extensions = {
       autolink:           true,
