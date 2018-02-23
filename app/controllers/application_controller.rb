cc230bb142f7237d7c9d469f53b89d27ef66d08b
John Wilger <john@johnwilger.com>
2018-02-23 23:59:31 +0000

    Adds halt! to controllers to stop action
    Used mainly to allow service callbacks to prevent default rendering.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,12 @@ require 'the_help/service_caller'
 require 'the_help/errors'
 
 class ApplicationController < ActionController::Base
+  class RequestHalted < RuntimeError; end
+
   include TheHelp::ServiceCaller
 
   protect_from_forgery with: :exception
+  around_action :catch_halt
   before_action :authenticate_user!
   rescue_from TheHelp::NotAuthorizedError, with: :render_forbidden
 
@@ -18,6 +21,16 @@ class ApplicationController < ActionController::Base
                  not_authorized: method(:render_forbidden))
   end
 
+  def catch_halt
+    code = catch(:halt) do
+      yield
+    end
+    unless performed?
+      raise RequestHalted,
+        'The request was halted before the response was constructed.'
+    end
+  end
+
   def render_forbidden
     respond_to do |format|
       format.html do
@@ -27,6 +40,7 @@ class ApplicationController < ActionController::Base
       format.xml { head :forbidden }
       format.any { head :forbidden }
     end
+    halt!
   end
 
   def render_404
@@ -38,5 +52,10 @@ class ApplicationController < ActionController::Base
       format.xml  { head :not_found }
       format.any  { head :not_found }
     end
+    halt!
+  end
+
+  def halt!
+    throw :halt
   end
 end
